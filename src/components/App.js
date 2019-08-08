b953e60a0bf0ab46f7a2750cfadf881f1491e375
zijies1 <546641464@qq.com>
2019-08-08 19:14:31 +1000

    home page with centered banner

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from '../logo.svg';
+import bg from '../assets/images/bg.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home" style = {{ "backgroundImage": `url(${bg})`}}>
+      <div className="dark-overlay"></div>
+
+      <div className="row home-row">
+        <div className="container d-flex justify-content-center align-items-center">
+          <div className="row pb-5">
+              <div className="text-white display-3">The Big Picture</div>
+              <p className="mt-5 text-white mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt voluptates rerum eveniet sapiente repellat esse, doloremque quod recusandae deleniti nostrum assumenda vel beatae sed aut modi nesciunt porro quisquam voluptatem.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
