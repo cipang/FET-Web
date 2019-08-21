727dba7cab88aaf5c48e9398c5adba7eefc63edf
zijies1 <546641464@qq.com>
2019-08-21 18:54:27 +1000

    add not found page

---
+++ src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppLayout from './layouts/AppLayout';
+
+class Explore extends React.Component {
+
+  render() {
+    return (
+      <AppLayout>
+        Not Found
+      </AppLayout>
+    );
+  }
+}
+
+export default Explore;
