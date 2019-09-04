bf41cfa2510b71be48e5bce3de21bee00958f446
zijies1 <546641464@qq.com>
2019-09-04 21:25:57 +1000

    simple draggable grid using draggable tabs

--- src/components/commons/DraggableTimetable.js
+++ src/components/commons/DraggableTimetable.js
@@ -1,4 +1,4 @@
-import { Tabs } from 'antd';
+import { Tabs, Row, Col } from 'antd';
 import React from 'react';
 import { DndProvider, DragSource, DropTarget } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
@@ -48,25 +48,26 @@ const WrapTabNode = DropTarget('DND_NODE', cardTarget, connect => ({
 
 class DraggableTabs extends React.Component {
   state = {
-    order: [],
+    order_row1:["1_1","1_2","1_3"],
+    order_row2:["2_1","2_2","2_3"],
+    order: ["1_1","1_2","1_3","2_1","2_2","2_3"],
   };
 
   moveTabNode = (dragKey, hoverKey) => {
     const newOrder = this.state.order.slice();
     const { children } = this.props;
-
-    React.Children.forEach(children, c => {
-      if (newOrder.indexOf(c.key) === -1) {
-        newOrder.push(c.key);
-      }
-    });
+    console.log("before:", newOrder);
 
     const dragIndex = newOrder.indexOf(dragKey);
     const hoverIndex = newOrder.indexOf(hoverKey);
+    console.log("newOrder[dragIndex]", dragIndex, newOrder[dragIndex]);
+    console.log("newOrder[hoverIndex]", hoverIndex, newOrder[hoverIndex]);
 
-    newOrder.splice(dragIndex, 1);
-    newOrder.splice(hoverIndex, 0, dragKey);
+    const tmp = newOrder[dragIndex];
+    newOrder[dragIndex] = newOrder[hoverIndex];
+    newOrder[hoverIndex] = tmp;
 
+    console.log("after:", newOrder);
     this.setState({
       order: newOrder,
     });
@@ -83,38 +84,73 @@ class DraggableTabs extends React.Component {
   );
 
   render() {
+    console.log("re render");
+
     const { order } = this.state;
     const { children } = this.props;
 
-    const tabs = [];
-    React.Children.forEach(children, c => {
-      tabs.push(c);
-    });
-
-    const orderTabs = tabs.slice().sort((a, b) => {
-      const orderA = order.indexOf(a.key);
-      const orderB = order.indexOf(b.key);
-
-      if (orderA !== -1 && orderB !== -1) {
-        return orderA - orderB;
+    const tabsList = [[],[]];
+    let tabCount = 0;
+    let tabsListCount = 0;
+    let tabs = [];
+    let len =  this.state.order.length;
+    for(let i = 0; i < len; i ++) {
+      React.Children.forEach(children, c => {
+        if(this.state.order[i] === c.key) {
+          let rowIndex = c.key.split("_")[0] - 1;
+          console.log(c.key);
+          tabsList[tabsListCount].push(c);
+          return;
+        }
+      });
+      tabCount += 1;
+      if(tabCount === 3) {
+        tabsListCount += 1;
       }
-      if (orderA !== -1) {
-        return -1;
-      }
-      if (orderB !== -1) {
-        return 1;
-      }
+    }
+    console.log(tabsList[0]);
+    console.log(tabsList[1]);
 
-      const ia = tabs.indexOf(a);
-      const ib = tabs.indexOf(b);
+    // React.Children.forEach(children, c => {
+    //   console.log(c.key, c.key.split("_"));
+    //   tabs.push(c);
+    //   tabCount += 1;
+    //   if(tabCount === 3) {
+    //     tabCount = 0;
+    //     tabsListCount += 1;
+    //     tabsList.push(tabs);
+    //     tabs = [];
+    //   }
+    // });
 
-      return ia - ib;
-    });
+    // const orderTabs = tabs => (tabs.slice().sort((a, b) => {
+    //   const orderA = order.indexOf(a.key);
+    //   const orderB = order.indexOf(b.key);
+    //   // console.log(orderA, orderB);
+    //
+    //   if (orderA !== -1 && orderB !== -1) {
+    //     return orderA - orderB;
+    //   }
+    //   if (orderA !== -1) {
+    //     return -1;
+    //   }
+    //   if (orderB !== -1) {
+    //     return 1;
+    //   }
+    //
+    //   const ia = tabs.indexOf(a);
+    //   const ib = tabs.indexOf(b);
+    //
+    //   return ia - ib;
+    // }));
 
     return (
       <DndProvider backend={HTML5Backend}>
         <Tabs renderTabBar={this.renderTabBar} {...this.props}>
-          {orderTabs}
+          {tabsList[0]}
+        </Tabs>
+        <Tabs renderTabBar={this.renderTabBar} {...this.props}>
+          {tabsList[1]}
         </Tabs>
       </DndProvider>
     );
@@ -125,15 +161,12 @@ class DraggableTimetable extends React.Component {
   render() {
     return (
       <DraggableTabs>
-        <TabPane tab="tab 1" key="1">
-          Content of Tab Pane 1
-        </TabPane>
-        <TabPane tab="tab 2" key="2">
-          Content of Tab Pane 2
-        </TabPane>
-        <TabPane tab="tab 3" key="3">
-          Content of Tab Pane 3
-        </TabPane>
+          <TabPane tab="tab 1" key="1_1"/>
+          <TabPane tab="tab 2" key="1_2"/>
+          <TabPane tab="tab 3" key="1_3"/>
+          <TabPane tab="tab 4" key="2_1"/>
+          <TabPane tab="tab 5" key="2_2"/>
+          <TabPane tab="tab 6" key="2_3"/>
       </DraggableTabs>
     )
   }
