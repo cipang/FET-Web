4973b41716e333e9aea1844af0895314ba237193
zijies1 <546641464@qq.com>
2019-10-26 16:20:39 +1100

    add basic jest webpack setup

---
+++ src/components/test/testApp.test.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+// import App from '../App';
+
+// describe('App', () => {
+//   test('snapshot renders', () => {
+//     const component = renderer.create(<App />);
+//     let tree = component.toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
+
+
+it('My Test Case', () => {
+  expect(true).toEqual(true);
+});
